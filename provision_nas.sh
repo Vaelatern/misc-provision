a2ff9efd4bc85c44f7143b64778b5b6a0e9b496d
Toyam Cox <Vaelatern@gmail.com>
2016-02-21 00:02:16 -0500

    Shellcheck fixes

--- provision_nas.sh
+++ provision_nas.sh
@@ -9,8 +9,8 @@
 # Also do badblocks checking
 
 for binary in zpool badblocks cryptsetup smartctl lsblk readlink openssl dd awk sed; do
-    if [ ! $(which $binary) ]; then
-        echo "Error: missing binary $binary. Script cannot continue." 1>&2
+    if [ ! "$(which "${binary}")" ]; then
+        echo "Error: missing binary ${binary}. Script cannot continue." 1>&2
         exit 1
     fi
 done
@@ -24,7 +24,7 @@ keyfile="/root/storage.key"
 size=512
 zpool_name="tank"
 zpool_vdev_size=6
-zpool_opts="-o ashift=12 -O 'compression=lz4' -O 'dedup=on'"
+zpool_opts=(-o ashift=12 -O 'compression=lz4' -O 'dedup=on')
 
 trsh_plain=1
 trsh_mirror=2
@@ -46,37 +46,37 @@ vdev_type() {
 }
 
 get_disk_id() {
-	[ -b $1 ] || exit 1;
+	[ -b "${1}" ] || exit 1;
 	for disk_id in /dev/disk/by-id/*; do
 		export disk_id
-		[ "$(readlink -f $disk_id)" = "${1}" ] && echo "${disk_id}" && exit 0
+		[ "$(readlink -f "${disk_id}")" = "${1}" ] && echo "${disk_id}" && exit 0
 	done
 }
 
 for disk in $drives; do
-	smartctl -t long "${disk}" 2>&1 >/dev/null && echo "Started smart on ${disk}"
-	badblocks -sv "${disk}" 2> /tmp/badblocks.${disk#/dev/} &
+	smartctl -t long "${disk}" >/dev/null 2>&1 && echo "Started smart on ${disk}"
+	badblocks -sv "${disk}" 2> "/tmp/${disk#/dev/}.badblocks" &
 done
 
 wait
 
-openssl rand -out $keyfile -base64 $(( 2**21 * 3/4 ))
+openssl rand -out "$keyfile" -base64 $(( 2**21 * 3/4 ))
 
 for disk in $drives; do
-	echo "YES" | cryptsetup -s $size -c $cipher -h $hash --key-file $keyfile luksFormat $disk
-	cryptsetup --key-file $keyfile open --type luks $disk "${zpool_name}-${disk#/dev/}"
-	disk_id="$(get_disk_id $disk)"
+	echo "YES" | cryptsetup -s "$size" -c "${cipher}" -h "${hash}" --key-file "${keyfile}" luksFormat "${disk}"
+	cryptsetup --key-file "${keyfile}" open --type luks "${disk}" "${zpool_name}-${disk#/dev/}"
+	disk_id="$(get_disk_id "${disk}")"
 	line="${zpool_name}-${disk#/dev/}	${disk_id}	${keyfile}"
 	echo "${line}" >> /etc/crypttab
 done
-chmod a-rwx $keyfile
+chmod a-rwx "${keyfile}"
 
 vdev_spec=""
 vdev=""
 
 for elem in $drives; do
 	[ -z "$vdev" ] && vdev="$(vdev_type) " && count=0
-	[ $count -lt $zpool_vdev_size ] && count=$(( $count+1 )) && vdev+="/dev/mapper/${zpool_name}-${elem#/dev/} "
+	[ $count -lt $zpool_vdev_size ] && count=$(( count+1 )) && vdev+="/dev/mapper/${zpool_name}-${elem#/dev/} "
 	[ $count -eq $zpool_vdev_size ] && vdev_spec+="$vdev" && vdev=""
 done
 
@@ -88,14 +88,14 @@ for disk in $drives; do
 done
 
 # Test run
-zpool create -n $zpool_opts $zpool_name $vdev_spec
+zpool create -n ${zpool_opts[@]} ${zpool_name} ${vdev_spec}
 
 echo -n "Continue by pressing y, cancel with ^c [Y] "
-read var
+read
 
 for disk in $drives; do
 	dd if=/dev/zero of="/dev/mapper/${zpool_name}-${disk#/dev/}" bs=1M
 done
 
-zpool create $zpool_opts $zpool_name $vdev_spec
+zpool create ${zpool_opts[@]} ${zpool_name} ${vdev_spec}
 
